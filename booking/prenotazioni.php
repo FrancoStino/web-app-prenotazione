5e09299c9ee7dfc174d78b10706be5229420f713
Davide <info@davideladisa.it>
2025-06-22 13:50:55 +0200

    Add reset for wrapperTavoli click event

--- booking/prenotazioni.php
+++ booking/prenotazioni.php
@@ -2,6 +2,5 @@
 $dati = file_get_contents('php://input');
 $dati = json_decode($dati);
 //Modifico il nome solo per vedere che funziona e restituisco i dati in formato json
-$dati->nome = "Luisa";
+// $dati->nome = "Luisa";
 echo json_encode($dati);
-?>
